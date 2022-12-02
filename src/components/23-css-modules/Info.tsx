583e1429dcf8a7d1121e269904016bb35d8a2403
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-12-02 19:43:56 +0100

    deploy

--- src/components/23-css-modules/Info.tsx
+++ src/components/23-css-modules/Info.tsx
@@ -7,6 +7,7 @@ console.log(styles)
 export const Info=()=>{
     return(
 <div className={styles.info}>
+    <span>hello</span>
     <h1>Hello from the Info component</h1>
     <button className={"my-button"}>Click me in the Info component</button>
 </div>
