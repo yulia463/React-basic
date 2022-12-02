db47d807913d8925dd20007090bb9ff2dcda48c5
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-12-02 17:27:29 +0100

    change Post and Posts

--- src/components/21-jsonplaceholder-posts/Post.tsx
+++ src/components/21-jsonplaceholder-posts/Post.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-type PostPropsType={
-  id:number,
-  body:string,
-  title:string,
-  userId:number
+type PostPropsType = {
+    id: number,
+    body: string,
+    title: string,
+    userId: number
 }
-export const Post = (props:PostPropsType) => {
-  return(
-      <div style={{marginBottom:"30px"}}>
-        {props.id }{props.body}{props.userId}{props.title}
-      </div>
-  )
+export const Post = (props: PostPropsType) => {
+    return (
+        <div style={{marginBottom: "30px"}}>
+            <small> {props.id}</small>
+            <h2>{props.body}</h2>
+            <p>{props.userId}</p>
+            <h3>{props.title}</h3>
+        </div>
+    )
 }
--- src/components/21-jsonplaceholder-posts/Posts.tsx
+++ src/components/21-jsonplaceholder-posts/Posts.tsx
@@ -1,26 +1,31 @@
 import React, {useEffect, useState} from "react";
 import {Post} from "./Post";
-
+import './Post.css'
 
 export const Posts=()=>{
     const [posts, setPosts] = useState([])
+    const [error,setError]=useState('')
+    const [isLoading,setIsLoading]=useState(true)
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
-            .then((posts)=>{
-                setPosts(posts)
-                console.log(posts)
-            })
-            .catch((error)=>console.log(error.message))
+            .then((posts)=>setPosts(posts))
+            .catch((error)=>setError(error.message))
+            .finally(()=>setIsLoading(false))
     }, [])
 
-    return(
-        <div>
-            {posts.map((el:any)=>{
-                return <Post id={el.id} body={el.body} title={el.title} userId={el.userId}/>
-            })}
+    if(error){
+        return <h1>Error:{error}</h1>
+    }
 
+    return(
+        <div className={"post"}>
+            <h1>Posts</h1>
+            <hr/>
+            {isLoading? <h1>Loading...</h1>:posts.map((el:any)=>{
+                    return <Post key={el.id} id={el.id} body={el.body} title={el.title} userId={el.userId}/>
+                })}
         </div>
     )
 }
