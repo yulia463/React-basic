dc1da94b773277855b6f528d087941ce78040b6c
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-12-04 15:26:30 +0100

    change Info.tsx

--- src/components/23-css-modules/Info.tsx
+++ src/components/23-css-modules/Info.tsx
@@ -4,13 +4,16 @@ import styles from './Info.module.css'
 
 console.log(styles)
 
-export const Info=()=>{
-    return(
-<div className={styles.info}>
-    <span>hello</span>
-    <h1>Hello from the Info component</h1>
-    <h2>h2 component</h2>
-    <button className={"my-button"}>Click me in the Info component</button>
-</div>
+export const Info = () => {
+    return (
+        <div className={styles.info}>
+            <span>hello</span>
+
+            <h1>Hello from the Info component</h1>
+            <h2>h2 component</h2>
+
+            <button className={"my-button"}>Click me in the Info component</button>
+
+        </div>
     )
 }
