85ebd115c07920db07cb331b36980537702b3269
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-12-15 16:18:20 +0100

    add About and Single components

--- src/components/24-react-router/About.tsx
+++ src/components/24-react-router/About.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
 const About = () => {
-    return (
-        <>
-            <h1>about</h1>
-        </>
-    )
+    return <h1>about</h1>
 }
 export default About;
--- src/components/24-react-router/SingleCourse.tsx
+++ src/components/24-react-router/SingleCourse.tsx
@@ -1,16 +1,24 @@
-import React from "react";
-import {Link, useParams} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Link, useNavigate, useParams} from "react-router-dom";
 import {coursesPropsType} from "./Courses";
 
-export const SingleCourse = (props:coursesPropsType) => {
+export const SingleCourse = (props: coursesPropsType) => {
     const params = useParams()
+    const navigate = useNavigate()
+
     const course = props.courses.find((course) => course.slug === params.courseSlug)
+    useEffect(() => {
+
+        if (!course) {
+            navigate('..', {relative: 'path'})
+        }
+    },[course,navigate])
     return (
         <>
             <h1>{course?.title}</h1>
             <h2>{course?.slug}</h2>
             <h3>{course?.id}</h3>
-            <Link to={".." } relative={"path"}>All courses</Link>
+            <Link to={".."} relative={"path"}>All courses</Link>
         </>
     )
 }
