310a2c3947e9465aa304455b793d00fb749d1271
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-12-17 12:47:54 +0100

    add Courses

--- src/components/24-react-router/Courses.tsx
+++ src/components/24-react-router/Courses.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import {Link, NavLink} from "react-router-dom";
-
+import React, {useEffect, useState} from "react";
+import {Link, NavLink, useLocation, useNavigate} from "react-router-dom";
+import queryString from 'query-string'
+import {coursesArray} from "./coursesArray";
 
 export type coursesPropsType = {
     courses: Array<{
@@ -9,14 +10,40 @@ export type coursesPropsType = {
         id: number;
     }>
 
+
 }
+
+export const SORT_KEYS = ['title', 'slug', 'id']
+
+export function sortCourses(courses: any, key: any) {
+    const sortedCourses = [...courses]
+    if (!key || !SORT_KEYS.includes(key)) {
+        return sortedCourses
+    }
+    sortedCourses.sort((a, b) => a[key] > b[key] ? 1 : -1)
+    return sortedCourses
+}
+
 export const Courses = (props: coursesPropsType) => {
+    const location = useLocation()
+    const query = queryString.parse(location.search)
+    const navigate = useNavigate()
+    const [sortKey, setSortKey] = useState(query.sort)
+    const [sortedCourses, setSortedCourses] = useState(
+        sortCourses(props.courses, sortKey)
+    )
+    useEffect(() => {
+    if(!SORT_KEYS.includes(sortKey)){
+        navigate('.')
+        setSortKey()
+        setSortedCourses([...coursesArray])
+    }
+    }, [sortKey, navigate])
     return (
         <>
-            <h1>Courses</h1>
-            {props.courses.map((course) => (
-                <div className={"courseLink"}  key={course.id}>
-
+            <h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
+            {sortedCourses.map((course: any) => (
+                <div className={"courseLink"} key={course.id}>
                     <Link to={course.slug}>
                         {course.title}
                     </Link>
